e8221b203dfafbf6978fd857a00f07eebb8cc08d
Ahmed <aaosman2@wisc.edu>
2023-10-09 08:59:54 -0500

    Added interfaces

---
+++ BackendInterface.java
@@ -0,0 +1,37 @@
+///////////////////////////////////////   
+// Title: Backend Interface
+// Course: CS 400 Fall 2023
+///////////////////////////////////////
+
+import java.util.Iterator;
+import java.io.File;
+
+/**
+ * An interface for the backend that exposes the required functionality to the frontend: read
+ * data from a file, get a list of meteorites with the maximum mass in the data set, get a list
+ * of meteorites with a mass between two specified thresholds.
+ *
+ * This interface extends the IterableMultiKeySortedCollectionInterface to iterate over the 
+ * keys and values stored in the data structure.
+ */
+public interface BackendInterface<T extends Comparable<T>> extends MeteoriteInterface<T>, 
+                 IterableMultiKeySortedCollectionInterface<T>, Iterable<T> {
+    /**
+     * Reads data from a CSV formatted file and stores it as a string.
+     * @param fileName the name of the file the data is to be read from. 
+     */
+    public void readData(File fileName);
+
+    /**
+     * @return a list of meteorites with the maximum mass in the data set.
+     */
+    public T[] listMaxMass();
+
+    /**
+     * @return a list of meteorites with a mass between two specified thresholds.
+     * @param lowerBound the lower bound of the specified threshold.
+     * @param higherBound the higher bound of the specified threshold.
+     */
+    public T[] listBoundedMass(double lowerBound, double higherBound);
+
+}
---
+++ FrontendInterface.java
@@ -0,0 +1,41 @@
+public interface FrontendInterface {
+ // Scanner input;
+ // Backend backend;
+
+ // public IndividualFrontendInterface(BackendInterface backend, Scanner input);
+
+    /**
+     * initialize the loop that repeatedly prompts users to choose a command, restarting after a command completes
+     */
+    void startLoop();
+
+    /**
+     * prompt the user for a .csv file with the scanner
+     * don't take method parameters, ask for the file location with the scanner
+     */
+    void loadFile(); // prompt user for file with scanner (don't take method parameters)
+
+    /**
+     * prompt the user for the upper/lower bounds with the scanner
+     */
+    void listBetweenMasses(); // prompt user for upper/lower bounds with scanner
+
+    /**
+     * prompt the user for a lower bound with the scanner or use a default
+     */
+    void listGreatestMasses(); // prompt user for a lower bound or use default
+
+    /**
+     * exit the program using System.exit()
+     */
+    void exit();
+
+    /**
+     * print an error for the user, formatted nicely
+     * @param error the error to display to the user
+     */
+    void printError(String error);
+
+
+}
+
